20949764d9c395668a2ff9b7c7e8b71f4c6ce9bd
Christian <Christianmnaira@gmail.com>
2025-05-02 11:39:27 +0800

    fix: added a score feature

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,6 +2,25 @@ import React, { useState } from 'react'
 
 const Result = ({userAnswers, contents}) => {
 
+  const [score, setScore] = useState(0);
+
+  
+
+  const handleResult = () => {
+ 
+    let newScore = 0;
+
+    contents.forEach((answerList, index) => {
+        if (answerList.answer === userAnswers[index]) {
+            newScore += 1;
+        }
+    })
+
+    setScore(newScore);
+
+  }
+
+
  
   return (
     <div className='container-sm mt-5 text-center border p-5 rounded'>
@@ -9,17 +28,17 @@ const Result = ({userAnswers, contents}) => {
         
         <h3 className='fs-2'>Your Answers: </h3>
         
-        <div className='border bg-primary rounded p-5 mt-2'>
+        <div className='border bg-success rounded p-5 mt-2'>
             {userAnswers.map((answer, key) => {
                 return <p key={key} className='fs-3 text-white'>{answer}</p>
             })}
         </div>
 
         <div>
-            <button type='button' className='btn btn-success fs-2 p-2 mt-5'>Get Result</button>
+            <button type='button' className='btn btn-primary fs-2 p-2 mt-5' onClick={handleResult}>Get Result</button>
         </div>
-        
-        <h2>{score}</h2>
+
+        <h2 className='mt-2 fs-2'>{score} / {contents.length}</h2>
 
     </div>
   )
