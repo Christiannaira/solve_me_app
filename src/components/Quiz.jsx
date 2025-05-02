83d182aa6106ab675a00614297c6b126acd3f816
Christian <Christianmnaira@gmail.com>
2025-05-02 09:00:40 +0800

    fix: states must be inside the body of the component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
-
-const contents = [
-    {question: "What is the main purpose of React?",
-     options: ["To style web pages", "To connect to databases", "To build user interfaces", "To manage server-side logic"],
-     answer: "To build user interfaces"
-    },
-    {question: "Which of the following is used to pass data from one component to another in React?",
-    options: ["state", "setState", "props", "render"],
-    answer: "props"
-       },
-]
+import React, { useState } from 'react';
 
 const Quiz = () => {
+
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+
+    const contents = [
+        {question: "What is the main purpose of React?",
+         options: ["To style web pages", "To connect to databases", "To build user interfaces", "To manage server-side logic"],
+         answer: "To build user interfaces"
+        },
+        {question: "Which of the following is used to pass data from one component to another in React?",
+        options: ["state", "setState", "props", "render"],
+        answer: "props"
+           },
+    ]
+
   return (
     <div>
-      {contents[0].question}
+      <h2>{contents[currentQuestion].question}</h2>
     </div>
   )
 }
