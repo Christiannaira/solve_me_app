fb34a95b5ea23a3e92d4879a132b3d43025e0b4f
Christian <Christianmnaira@gmail.com>
2025-05-13 09:47:52 +0800

    fix: checked the result of the api in the console

--- src/components/ApiTesting.jsx
+++ src/components/ApiTesting.jsx
@@ -1,36 +1,42 @@
 import { useEffect } from "react";
 import axios from "axios";
 
-
 const ApiTesting = () => {
 
-  
-    const options = {
-    method: 'GET',
-    url: 'https://500-quiz-api.p.rapidapi.com/api',
-    headers: {
-        'x-rapidapi-key': '1e8571aaf4msh6c0ad5a8f8c2affp1e9d7ejsnc63beb1b3a52',
-        'x-rapidapi-host': '500-quiz-api.p.rapidapi.com'
-    }
+  useEffect(() => {
+
+    const fetchQuiz = async () => {
+
+      try {
+        const options = {
+          method: 'GET',
+          url: 'https://500-quiz-api.p.rapidapi.com/api?limit=5',
+          headers: {
+            'x-rapidapi-key': '1e8571aaf4msh6c0ad5a8f8c2affp1e9d7ejsnc63beb1b3a52',
+            'x-rapidapi-host': '500-quiz-api.p.rapidapi.com'
+          }
+        };
+
+        const response = await axios.request(options);
+        console.log(response.data); 
+        
+
+      } catch (error) {
+        console.error("API error:", error);
+      }
+
     };
 
-    useEffect(() => {
-        try {
-	        const response = axios.request(options);
-	        console.log(response.data);
-        } catch (error) {
-            console.error(error);
-        }
-    }, []);
+    fetchQuiz(); 
+    
+
+  }, []);
 
   return (
-
     <div>
-        
-        
-        
+      <h2>Check console for quiz data</h2>
     </div>
-  )
-}
+  );
+};
 
-export default ApiTesting
+export default ApiTesting;
